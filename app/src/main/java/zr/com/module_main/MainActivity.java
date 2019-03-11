b4c5893cd168ffc43f9ff4ba0b940b078e950dd3
yushiwo <zhengrui@otcbtc.com>
2019-03-11 11:13:36 +0800

    proj init

---
+++ app/src/main/java/zr/com/module_main/MainActivity.java
@@ -0,0 +1,24 @@
+package zr.com.module_main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import zr.com.module_lib.LibTest;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mContentTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+        mContentTextView = findViewById(R.id.tv_content);
+        mContentTextView.setText(LibTest.getLibStr());
+    }
+}
